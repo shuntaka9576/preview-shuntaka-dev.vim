1c046f0918cb3759799c899e6033a65e89fe389f
shuntaka9576 <shuntaka9576@gmail.com>
2022-12-23 18:35:05 +0900

    Adjustment CSS

--- denops/hozidev/template.ts
+++ denops/hozidev/template.ts
@@ -2,76 +2,96 @@ import { html } from "https://deno.land/x/hono/middleware.ts";
 
 export const template = (params: { port: number }) =>
   html`<html>
-  <head>
-    <title>Preview |</title>
-  </head>
-  <style>
-    html,
-    body {
-      padding: 0;
-      margin: 0;
-      font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-        Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-    }
+    <head>
+      <title>Preview |</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link
+        href="https://unpkg.com/@hozi-dev/content-css@0.6.0/build/index.css"
+        rel="stylesheet"
+      />
+    </head>
+    <style>
+      html {
+        height: 100%;
+      }
+      body {
+        padding: 0;
+        margin: 0;
+        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+      }
 
-    a {
-      color: inherit;
-      text-decoration: none;
-    }
+      :root {
+        --bg-color: #f7fafc;
+        --header-color: #fffefc;
+        --text-color: #525457;
+        --article-record-underline: rgba(162, 177, 202, 0.3);
+        --article-area-color: #fff;
+        --toc-list-color: rgba(162, 177, 202, 0.3);
+        --toc-list-text-color: #57595b87;
+        --toc-list-color-active: #525457;
+      }
 
-    * {
-      box-sizing: border-box;
-    }
+      a {
+        color: inherit;
+        text-decoration: none;
+      }
 
-    #base {
-      padding: 8px 0px 0px 0px;
-      width: 100%;
-      word-break: break-all;
-    }
+      * {
+        box-sizing: border-box;
+        max-width: 100%;
+      }
 
-    #editor {
-      max-width: 800px;
-      padding: 0px 24px 0px 24px;
-      margin: 0 auto;
-    }
+      .base {
+        padding: 8px 0px 0px 0px;
+        width: 100%;
+        word-break: break-all;
+      }
 
-    #content {
-    }
+      .editor {
+        display: flex;
+        justify-content: space-between;
+        max-width: 800px;
+        padding: 0px 24px 0px 24px;
+        margin: 0 auto;
+      }
 
-    #title {
-      text-align: center;
-    }
-  </style>
-  <link
-    href="https://unpkg.com/@hozi-dev/content-css@0.6.0/build/index.css"
-    rel="stylesheet"
-  />
-  <script src="https://code.jquery.com/jquery-1.11.1.js"></script>
-  <script>
-    const receiveMessage = ({ data }) => {
-      const article = JSON.parse(data);
-      document.title = "Preview | " + article.title;
-      $("#html").html(article.content);
-      $("#title").text(article.title);
-    };
+      .content {
+        max-width: 800px;
+      }
 
-    window.onload = () => {
-      socket = new WebSocket("ws://localhost:" + ${params.port} + "/ws");
-      socket.addEventListener("message", receiveMessage);
-    };
-  </script>
-  <body>
-    <div id="base">
-      <div id="title"></div>
-      <hr />
-      <br />
-      <div id="editor">
-        <div id="content">
-          <div id="hozi-dev-article-content-light">
-            <div id="html"></div>
+      .title {
+        text-align: center;
+      }
+
+      .hozi-dev-article-content-light {
+        max-width: 800px;
+      }
+    </style>
+    <script src="https://code.jquery.com/jquery-1.11.1.js"></script>
+    <script>
+      const receiveMessage = ({ data }) => {
+        const article = JSON.parse(data);
+        document.title = "Preview | " + article.title;
+        $(".hozi-dev-article-content-light").html(article.content);
+        $(".title").text(article.title);
+      };
+
+      window.onload = () => {
+        socket = new WebSocket("ws://localhost:" + ${params.port} + "/ws");
+        socket.addEventListener("message", receiveMessage);
+      };
+    </script>
+    <body>
+      <div class="base">
+        <div class="title"></div>
+        <hr />
+        <br />
+        <div class="editor">
+          <div class="content">
+            <div class="hozi-dev-article-content-light"></div>
           </div>
         </div>
       </div>
-    </div>
-  </body>
-</html>`;
+    </body>
+  </html>`;
